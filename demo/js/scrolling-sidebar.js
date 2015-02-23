9109b89ba6038506670d1c3d94190ab41dd214d7
Jason Alvis <hello@jasonalvis.co.uk>
2015-02-23 23:28:29 +0000

    working on the js

--- demo/js/scrolling-sidebar.js
+++ demo/js/scrolling-sidebar.js
@@ -13,7 +13,13 @@ var FisheyeScrollSidebar = (function ($) {
 
     return {
         settings: {
+            header:         $("header"),
+            footer:         $("footer"),
+            sidebar:        $(".column.left"),
 
+            spacing:        20,
+            lastScrollTop:  0,
+            lockedTo:       "none"
         },
 
         init: function() {
@@ -22,8 +28,56 @@ var FisheyeScrollSidebar = (function ($) {
             this.bindWindowActions();
         },
 
+        detectDirection: function(){
+            var st = window.pageYOffset,
+                direction;
+
+            if (st > s.lastScrollTop) {
+                direction = "down";
+            } else {
+                direction = "up";
+            }
+
+            s.lastScrollTop = st;
+
+            return direction;
+        },
+
         fixSidebar: function(){
-            console.log("called");
+            // if sidebar doesn't exist return
+            if(!s.sidebar.length){
+                return;
+            }
+
+            var direction       = FisheyeScrollSidebar.detectDirection(),
+                scrollPosition  = $(window).scrollTop(),
+
+                headerHeight    = s.header.outerHeight(true),
+
+                sidebarHeight   = s.sidebar.outerHeight(true),
+                sidebarPosition = s.sidebar.position(),
+                sidebarOffset   = s.sidebar.offset();
+
+
+            if(scrollPosition > headerHeight){
+                // bottom of sidebar reached
+                if(direction === "down" && s.lockedTo === "none" && ($(window).height() + scrollPosition) > (sidebarOffset.top + sidebarHeight + s.spacing) ){
+                    s.sidebar.css({
+                        position: "fixed",
+                        top: "auto",
+                        bottom: s.spacing + "px"
+                    });
+
+                    s.lockedTo = "bottom";
+                }
+
+                // footer reached, end of the line
+                if( (scrollPosition + sidebarHeight) > () ){
+
+                }
+
+
+            }
         },
 
         bindWindowActions: function(){
