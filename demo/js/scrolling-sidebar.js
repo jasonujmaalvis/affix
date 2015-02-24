b38ffa59c0a27f67e1a4c6dadc3545ca048c5364
Jason Alvis <jasona@fisheyehq.com>
2015-02-24 17:29:03 +0000

    working on the javascript

--- demo/js/scrolling-sidebar.js
+++ demo/js/scrolling-sidebar.js
@@ -15,11 +15,13 @@ var FisheyeScrollSidebar = (function ($) {
         settings: {
             header:         $("header"),
             footer:         $("footer"),
+            main:           $(".row-main"),
             sidebar:        $(".column.left"),
 
             spacing:        20,
             lastScrollTop:  0,
-            lockedTo:       "none"
+            lockedTo:       "none",
+            endReached:     false
         },
 
         init: function() {
@@ -53,13 +55,35 @@ var FisheyeScrollSidebar = (function ($) {
                 scrollPosition  = $(window).scrollTop(),
 
                 headerHeight    = s.header.outerHeight(true),
+                footerOffset    = s.footer.offset(),
 
                 sidebarHeight   = s.sidebar.outerHeight(true),
-                sidebarPosition = s.sidebar.position(),
                 sidebarOffset   = s.sidebar.offset();
 
+            // footer reached, end of the line (80 for footer margin-top)
+            if( (sidebarOffset.top + sidebarHeight) >= footerOffset.top - 80){
+                if(direction === "down" && s.endReached === false){
+                    s.sidebar.css({
+                        position: "absolute",
+                        top: "auto",
+                        bottom: "0px"
+                    });
 
-            if(scrollPosition > headerHeight){
+                    s.lockedTo   = "none";
+                    s.endReached = true;
+                }
+
+                if(direction === "up" && sidebarOffset.top >= scrollPosition){
+                    s.sidebar.css({
+                        position: "fixed",
+                        top: "0px",
+                        bottom: "auto"
+                    });
+
+                    s.lockedTo   = "top";
+                    s.endReached = false;
+                }
+            } else if(scrollPosition > headerHeight && s.endReached === false){
                 // bottom of sidebar reached
                 if(direction === "down" && s.lockedTo === "none" && ($(window).height() + scrollPosition) > (sidebarOffset.top + sidebarHeight + s.spacing) ){
                     s.sidebar.css({
@@ -69,14 +93,45 @@ var FisheyeScrollSidebar = (function ($) {
                     });
 
                     s.lockedTo = "bottom";
+                // if sidebar is fixed to top and we scroll down absolute the sidebar so they don't move
+                } else if(direction === "down" && s.lockedTo === "top"){
+                    s.sidebar.css({
+                        position: "absolute",
+                        top: (sidebarOffset.top - s.main.offset().top) + "px",
+                        bottom: "auto"
+                    });
+
+                    s.lockedTo = "none";
+                // top of sidebar reached
+                } else if(direction === "up" && s.lockedTo === "none" && sidebarOffset.top >= scrollPosition){
+                    s.sidebar.css({
+                        position: "fixed",
+                        top: "0px",
+                        bottom: "auto"
+                    });
+
+                    s.lockedTo = "top";
+                // if sidebar is fixed to bottom and we scroll up absolute the sidebar so they don't move
+                } else if(direction === "up" && s.lockedTo === "bottom") {
+                    s.sidebar.css({
+                        position: "absolute",
+                        top: (sidebarOffset.top - s.main.offset().top) + "px",
+                        bottom: "auto"
+                    });
+
+                    s.lockedTo = "none";
                 }
+            } else {
+                // top of sidebar reached, start of the line
+                if(s.sidebar.css("position") !== "relative"){
+                    s.sidebar.css({
+                        position: "relative",
+                        top: "auto",
+                        bottom: "auto"
+                    });
 
-                // footer reached, end of the line
-                if( (scrollPosition + sidebarHeight) > () ){
-
+                    s.lockedTo = "none";
                 }
-
-
             }
         },
 
