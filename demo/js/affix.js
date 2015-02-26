c324dc6f8b4bae8c6e0617c40ed40d0c39bdd44b
Jason Alvis <jasona@fisheyehq.com>
2015-02-26 09:18:08 +0000

    added app.js

--- demo/js/affix.js
+++ demo/js/affix.js
@@ -10,16 +10,46 @@
 
     $.fn.affix = function(options) {
         // passing destruct: true allows
-        var settings = $j.extend({
+        var settings = $.extend({
             destruct: false
         }, options);
 
         return this.each(function() {
 
-            // accessing settings
-            // settings.destruct
+            console.log("called");
 
         });
     };
 
 })(jQuery);
+
+// example from the RWD magento theme, left just for reference (will be removed eventually)
+// jQuery.fn.toggleSingle = function (options) {
+
+//     // passing destruct: true allows
+//     var settings = $j.extend({
+//         destruct: false
+//     }, options);
+
+//     return this.each(function () {
+//         if (!settings.destruct) {
+//             $j(this).on('click', function () {
+//                 $j(this)
+//                     .toggleClass('active')
+//                     .next()
+//                     .toggleClass('no-display');
+//             });
+//             // Hide the content
+//             $j(this).next().addClass('no-display');
+//         } else {
+//             // Remove event handler so that the toggle link can no longer be used
+//             $j(this).off('click');
+//             // Remove all classes that were added by this plugin
+//             $j(this)
+//                 .removeClass('active')
+//                 .next()
+//                 .removeClass('no-display');
+//         }
+
+//     });
+// }
---
+++ demo/js/app.js
@@ -0,0 +1,13 @@
+/**
+*
+* @filename     app.js
+* @author       Jason Alvis
+*
+*/
+jQuery(document).ready(function() {
+
+    $(".column.left").affix({
+
+    });
+
+});
